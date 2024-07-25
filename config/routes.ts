06703e4f9ba25bcbdc8096d4c0177367f5e61c9f
ChloeLin <chloelin@ssw.com.au>
2024-07-25 14:48:33 +0800

    新增表单页

--- config/routes.ts
+++ config/routes.ts
@@ -6,6 +6,7 @@ export default [
   },
   { path: '/welcome', name: '欢迎', icon: 'smile', component: './Welcome' },
   { name: '查询表格', icon: 'table', path: '/list', component: './TableList' },
+  { name: '表单', icon: 'form', path: '/form', component: './Form' },
   { path: '/', redirect: '/welcome' },
   { path: '*', layout: false, component: './404' },
 ];
